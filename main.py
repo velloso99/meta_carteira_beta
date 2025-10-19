2abcab989bed3a65e91e18ba7fc728c40ea25c19
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-10-19 12:15:43 -0300

    main

--- main.py
+++ main.py
@@ -23,20 +23,22 @@ janela.geometry(f"{largura_root}x{altura_root}+{pos_x}+{pos_y}")
 #********************************************************************************************************
 #Frames
 
-frame_cima= Frame(janela, width=850, height=50, bg=co5)
+frame_cima= Frame(janela, width=850, height=50, bg=co9)
 frame_cima.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW)
 
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=1, columnspan=1, ipadx=680 )
 
-frame_meio= Frame(janela, width=850, height=570, bg=co5)
+frame_meio= Frame(janela, width=850, height=50, bg=co9)
 frame_meio.grid(row=2, column=0, pady=0, padx=0, sticky=NSEW)
 
-frame_baixo= Frame(janela, width=850, height=285, bg=co5)
+ttk.Separator(janela, orient=HORIZONTAL).grid(row=3, columnspan=1, ipadx=680 )
+
+frame_baixo= Frame(janela, width=850, height=285, bg=co9)
 frame_baixo.grid(row=4, column=0, pady=0, padx=0, sticky=NSEW)
 #*******************************************************************************************************
 #Função de Controle
 def control(i):
-    #Cadastro de clientes
+    #Cadastro 
     if i =='cadastro':
         for widget in frame_cima.winfo_children():
             widget.destroy()
@@ -46,7 +48,16 @@ def control(i):
             widget.destroy()
         #chamar a função
         cadastro()
-
+    #Cadastro de clientes
+    if i =='cadastro clientes':
+        for widget in frame_cima.winfo_children():
+            widget.destroy()
+        for widget in frame_meio.winfo_children():
+            widget.destroy()
+        for widget in frame_baixo.winfo_children():
+            widget.destroy()
+        #chamar a função
+        cadastro_clientes()
 
 
 
@@ -59,7 +70,7 @@ def control(i):
 #*******************************************************************************************************
 #Titulo
 
-t_titulo= Label(frame_cima, text=("Painel"), font=('Ivy 20 bold'), bg=co5, fg=co11)
+t_titulo= Label(frame_cima, text=("Painel"), font=('Ivy 20 bold'), bg=co9, fg=co11)
 t_titulo.place(x=425, y=23, anchor=CENTER)
 
 
@@ -70,14 +81,14 @@ t_titulo.place(x=425, y=23, anchor=CENTER)
 #********************************************************************************************************
 #Botões
 
-bt_Cadastro= Button(frame_meio,command=lambda:control('cadastro'), text="Cadastro", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-bt_Cadastro.place(x=10, y=10)
+bt_Cadastro= Button(frame_meio,command=lambda:control('cadastro'), text="Cadastro", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+bt_Cadastro.grid(row=0, column=0)
 
-bt_atualizar= Button(frame_meio, text="Atualizar", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-bt_atualizar.place(x=95, y=10)
+bt_atualizar= Button(frame_meio, text="Atualizar", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+bt_atualizar.grid(row=0, column=1)
 
-bt_clientes= Button(frame_meio, text="Clientes", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-bt_clientes.place(x=180, y=10)
+bt_clientes= Button(frame_meio, text="Clientes", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+bt_clientes.grid(row=0, column=2)
 
 
 
@@ -85,25 +96,49 @@ bt_clientes.place(x=180, y=10)
 #Cadastro
 def cadastro():
 
-    t_titulo= Label(frame_cima, text=("Cadastro em Geral"), font=('Ivy 20 bold'), bg=co5, fg=co11)
+    t_titulo= Label(frame_cima, text=("Cadastro em Geral"), font=('Ivy 20 bold'), bg=co9, fg=co11)
     t_titulo.place(x=425, y=23, anchor=CENTER)
 
     
-    bt_Cadastro= Button(frame_meio, text="Cadastro Clientes", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-    bt_Cadastro.place(x=10, y=10)
+    bt_Cadastro= Button(frame_meio, command=lambda:control('cadastro clientes'), text="Cadastro Clientes", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_Cadastro.grid(row=0, column=0)
 
-    bt_atualizar= Button(frame_meio, text="Cadastro Meta", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-    bt_atualizar.place(x=155, y=10)
+    bt_atualizar= Button(frame_meio, text="Cadastro Meta", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_atualizar.grid(row=0, column=1)
 
-    bt_clientes= Button(frame_meio, text="Cadstro ", bd=3, bg=co8, fg=co11, font=('verdana 10 bold'))
-    bt_clientes.place(x=280, y=10)
+    bt_clientes= Button(frame_meio, text="Cadstro ", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_clientes.grid(row=0, column=2)
 
+#******************************************************************************************************
+# Cadastrar Clientes
+def cadastro_clientes():
 
+    t_titulo= Label(frame_cima, text=("Cadastro Clientes"), font=('Ivy 20 bold'), bg=co9, fg=co11)
+    t_titulo.place(x=425, y=23, anchor=CENTER)
 
+    bt_Cadastro= Button(frame_meio, text="Salvar", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_Cadastro.grid(row=0, column=0)
 
+    bt_atualizar= Button(frame_meio, text="Atualizar", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_atualizar.grid(row=0, column=1)
 
+    bt_clientes= Button(frame_meio, text="Excluir", bd=3, bg=co9, fg=co11, font=('verdana 10 bold'))
+    bt_clientes.grid(row=0, column=2) 
 
+    l_matricula = Label(frame_baixo, text="Matricula:", font=('Ivy 10 bold'), bg=co9, fg=co11)
+    l_matricula.place(x=10, y=10)
+    e_matricula= Entry(frame_baixo, width=15, justify=LEFT, font=('Ivy 10 bold'),  relief='solid')
+    e_matricula.place(x=95, y=10)
 
+    l_raz_social = Label(frame_baixo, text="Razão Social:", font=('Ivy 10 bold'), bg=co9, fg=co11)
+    l_raz_social.place(x=10, y=50)
+    e_raz_social= Entry(frame_baixo, width=35, justify=LEFT, font=('Ivy 10 bold'),  relief='solid')
+    e_raz_social.place(x=105, y=50)
+
+    l_fantasia = Label(frame_baixo, text="Nome Fantasia:", font=('Ivy 10 bold'), bg=co9, fg=co11)
+    l_fantasia.place(x=10, y=90)
+    e_fantasia= Entry(frame_baixo, width=35, justify=LEFT, font=('Ivy 10 bold'),  relief='solid')
+    e_fantasia.place(x=115, y=90)
 
 
 
